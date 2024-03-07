bf1e0baf5b3e038fd2dad68b1975fbdc733c2f6d
wallace.drew <wallace.drew@gmail.com>
2024-03-07 14:58:09 -0500

    refacor tests

--- src/ScoreFormatter.test.ts
+++ src/ScoreFormatter.test.ts
@@ -1,27 +1,27 @@
 import {ScoreFormatter} from './ScoreFormatter';
 
 describe('ScoreFormatter', () => {
-    it('format 1 point with leading zeros to 3 characters', () => {
+    let scoreFormatter: ScoreFormatter;
 
-        const scoreFormatter = new ScoreFormatter();
+    beforeEach(() => {
+        scoreFormatter = new ScoreFormatter();
+    });
+
+    it('format 1 point with leading zeros to 3 characters', () => {
 
         const score = scoreFormatter.formatScore(1);
 
         expect(score).toBe('001');
     });
-    
-    it('format 2 points with leading zeros to 3 characters', () => {
 
-        const scoreFormatter = new ScoreFormatter();
+    it('format 2 points with leading zeros to 3 characters', () => {
 
         const score = scoreFormatter.formatScore(2);
 
         expect(score).toBe('002');
     });
-    
-    it('format 10 points with leading zeros to 3 characters', () => {
 
-        const scoreFormatter = new ScoreFormatter();
+    it('format 10 points with leading zeros to 3 characters', () => {
 
         const score = scoreFormatter.formatScore(10);
 
--- src/ScoreFormatter.ts
+++ src/ScoreFormatter.ts
@@ -1,7 +1,7 @@
 export class ScoreFormatter {
     public formatScore(score: number): string {
-        if(score > 9) {
-            return `0${score}`
+        if (score > 9) {
+            return `0${score}`;
         }
         return `00${score}`;
     }
