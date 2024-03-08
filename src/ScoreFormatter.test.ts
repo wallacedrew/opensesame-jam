f78fba45ef24bfb72ef613200667752a967695f8
wallace.drew <wallace.drew@gmail.com>
2024-03-07 20:51:14 -0500

    format code

--- src/ScoreFormatter.test.ts
+++ src/ScoreFormatter.test.ts
@@ -8,37 +8,27 @@ describe('ScoreFormatter', () => {
     });
 
     it('format 1 point with leading zeros to 3 characters', () => {
-
         const score = scoreFormatter.formatScore(1);
-
         expect(score).toBe('001');
     });
 
     it('format 2 points with leading zeros to 3 characters', () => {
-
         const score = scoreFormatter.formatScore(2);
-
         expect(score).toBe('002');
     });
 
     it('format 10 points with leading zeros to 3 characters', () => {
-
         const score = scoreFormatter.formatScore(10);
-
         expect(score).toBe('010');
     });
 
     it('format 100 points with leading zeros to 3 characters', () => {
-
         const score = scoreFormatter.formatScore(100);
-
         expect(score).toBe('100');
     });
 
     it('format 1000 points with to return 000', () => {
-
         const score = scoreFormatter.formatScore(1000);
-
         expect(score).toBe('000');
     });
 });
--- src/ScoreKeeper.test.ts
+++ src/ScoreKeeper.test.ts
@@ -9,74 +9,51 @@ describe('ScoreKeeper', () => {
     });
 
     it('outputs the initial score', () => {
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('000:000');
     });
 
     it('adds one to team A score', () => {
-
         scoreboard.scoreTeamA1();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('001:000');
     });
 
     it('adds one to team B score', () => {
-
         scoreboard.scoreTeamB1();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('000:001');
     });
 
     it('adds two to team A score', () => {
-
         scoreboard.scoreTeamA2();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('002:000');
     });
 
     it('adds two to team B score', () => {
-
         scoreboard.scoreTeamB2();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('000:002');
     });
 
     it('adds three to team A score', () => {
-
         scoreboard.scoreTeamA3();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('003:000');
     });
 
     it('adds three to team B score', () => {
-
         scoreboard.scoreTeamB3();
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('000:003');
     });
 
     it('displays 000 if the score is 1000', () => {
-
         for (let i = 0; i < 1000; i++) {
             scoreboard.scoreTeamA1();
         }
-
         const currentScore = scoreboard.getScore();
-
         expect(currentScore).toBe('000:000');
     });
 });
